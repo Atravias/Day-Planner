c71c3051e1546d987f6126298a3164b35d88d00c
Atravias Borley <atravias@gmail.com>
2019-12-19 22:07:25 -0700

    finished day planner. added js to grab ids and give them attributes

---
+++ script.js
@@ -0,0 +1,36 @@
+var time = document.getElementById("currentDay").append(new Date());
+var timeHour = document.getElementById("currentHour").append("The current hour of the day is: " + new Date().getHours());
+var input = document.querySelector("input");
+
+
+function hoursUpdater() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        var storageKey = $(this).attr("id");
+        var display = localStorage.getItem(storageKey);
+        $(this).siblings("input").val(display)
+        if (blockHour <= currentHour) {
+            $(this).addClass("past")
+        } else if (blockHour > currentHour) {
+            $(this).addClass("future")
+        } else {
+            return;
+        }
+        if (blockHour === currentHour) {
+            $(this).addClass("present")
+        } else {
+            return;
+        };
+    });
+};
+hoursUpdater();
+function minutesUpdater() {
+    var currentMinute = moment().minute();
+}
+minutesUpdater();
+$(".saveBtn").on("click", function (e) {
+    e.preventDefault();
+    localStorage.setItem($(this).siblings().attr("id"), $(this).siblings("input").val())
+    var save = $(this).siblings()
+})
